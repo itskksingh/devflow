d496e4a11b81aa861bc179d65b35ec14790f3875
Kishangit config --global user.email <realkishanpyare@gmail.com>
2024-12-27 00:40:57 +0530

    Esling & Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 const Home = () => {
-  return <h1 className='text-2xl text-violet-700 font-bold'>Welcome to the world of Next.js</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-black text-white">Welcome to the world of Next.js</h1>
+    </>
+  );
 };
 
 export default Home;
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,12 +5,47 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+const compat = new FlatCompat({ baseDirectory: __dirname });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default {
+  extends: [
+    ...compat.extends(
+      "next/core-web-vitals",
+      "next/typescript",
+      "standard",
+      "plugin:tailwindcss/recommended",
+      "prettier"
+    ),
+  ],
+  plugins: ["import"],
+  rules: {
+    "import/order": [
+      "error",
+      {
+        groups: ["builtin", "external", "internal", ["parent", "sibling"], "index", "object"],
+        "newlines-between": "always",
+        pathGroups: [
+          {
+            pattern: "@app/**",
+            group: "external",
+            position: "after",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["builtin"],
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+  ignorePatterns: ["components/ui/**"],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      rules: {
+        "no-undef": "off",
+      },
+    },
+  ],
+};

b3a02cc6fb941047c323a037ffa184393668c3b4
Kishangit config --global user.email <realkishanpyare@gmail.com>
2025-04-07 22:58:08 +0530

    Set up Light & Dark Theme, Shadcn, Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import React from "react";
-
-const Home = () => {
-  return <h1 className='text-3xl text-white font-black'>Welcome to the world of Next.js</h1>;
+const HomePage = () => {
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100'>
+      <h1 className='text-4xl font-bold text-primary-500 font-space-grotesk'>Hello Kishan</h1>
+    </div>
+  );
 };
 
-export default Home;
+export default HomePage;
